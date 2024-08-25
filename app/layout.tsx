3148200ae80167e3b2c05c44ba7ffabb15182518
dovicie <hyokoyama8810@gmail.com>
2024-08-26 08:45:32 +0900

    ✨ useTransitionを使ったstate更新処理の追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} mx-auto w-full max-w-screen-sm py-8`}
+      >
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,70 @@
-import Image from "next/image";
+"use client";
+
+import { memo, useState, useTransition } from "react";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm text-center sm:text-left">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isPending, startTransition] = useTransition();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const changePage = (newPage: number) => {
+    startTransition(() => {
+      setCurrentPage(newPage);
+    });
+  };
+
+  return (
+    <div className="space-y-4">
+      <div className="flex gap-x-4 p-4">
+        <button
+          className={currentPage === 1 ? "font-bold" : ""}
+          onClick={() => {
+            changePage(1);
+          }}
         >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          ページ1
+        </button>
+        <button
+          className={currentPage === 2 ? "font-bold" : ""}
+          onClick={() => {
+            changePage(2);
+          }}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          ページ2
+        </button>
+        <button
+          className={`${currentPage === 3 ? "font-bold" : ""}  ${isPending ? "opacity-50" : ""}`}
+          onClick={() => {
+            changePage(3);
+          }}
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          ページ3（激重）
+        </button>
+      </div>
+      <div className="border border-gray-200 p-4">
+        {currentPage === 1 && <FastPage pageNumber={1} />}
+        {currentPage === 2 && <FastPage pageNumber={2} />}
+        {currentPage === 3 && <SlowPage />}
+      </div>
     </div>
   );
 }
+
+// レンダリングに少なくとも1秒かかるコンポーネント
+const SlowPage = memo(function SlowPage() {
+  return (
+    <ul>
+      {Array.from({ length: 1000 }, (_, i) => (
+        <SlowListItem key={i} index={i} />
+      ))}
+    </ul>
+  );
+});
+const SlowListItem = ({ index }: { index: number }) => {
+  let startTime = performance.now();
+  while (performance.now() - startTime < 1) {}
+  return <li>#{index + 1}</li>;
+};
+
+const FastPage = ({ pageNumber }: { pageNumber: number }) => {
+  return <p>現在のページは{pageNumber}</p>;
+};
